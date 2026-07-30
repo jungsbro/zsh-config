b12253829d9b035c95a38ce374d22d04bfa045b8
Brian Jung <jungsbro@gmail.com>
2026-07-30 14:25:51 +0900

    [fix] fzf, rg, fd, zoxide를 정리했다.

--- .config/zsh/find.sh
+++ .config/zsh/find.sh
@@ -2,46 +2,56 @@
 #!/bin/bash
 
 # fd ===========================================================================
-alias fd=fdfind
+# arch
+# /usr/bin/fd
+
+# debian / ubuntu
+# /usr/bin/fdfind
+if [[ -f "/usr/bin/fdfind" ]]; then
+    alias fd=fdfind
+fi
+
+# fedora / rhel
+# /usr/bin/fd
 # ==============================================================================
 
 # fasd =========================================================================
-alias a='fasd -a'        # any
-alias s='fasd -si'       # show / search / select
-alias d='fasd -d'        # directory
-alias f='fasd -f'        # file
-alias sd='fasd -sid'     # interactive directory selection
-alias sf='fasd -sif'     # interactive file selection
-alias z='fasd_cd -d'     # cd, same functionality as j in autojump
-alias zz='fasd_cd -d -i' # cd with interactive selection
+# alias a='fasd -a'        # any
+# alias s='fasd -si'       # show / search / select
+# alias d='fasd -d'        # directory
+# alias f='fasd -f'        # file
+# alias sd='fasd -sid'     # interactive directory selection
+# alias sf='fasd -sif'     # interactive file selection
+# alias z='fasd_cd -d'     # cd, same functionality as j in autojump
+# alias zz='fasd_cd -d -i' # cd with interactive selection
 
-alias v='f -e vim' # quick opening files with vim
+# alias v='f -e vim' # quick opening files with vim
 # alias m='f -e mplayer' # quick opening files with mplayer
 # alias o='a -e xdg-open' # quick opening files with xdg-open
 # ==============================================================================
 
 # fzf ==========================================================================
 # export FZF_DEFAULT_COMMAND='fd -type f'
-export FZF_DEFAULT_COMMAND="find -L"
+# export FZF_DEFAULT_COMMAND="find -L"
 
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-
-
-function fzfv()
-{
-    fzf --preview '[[ $(file --mime {}) =~ binary ]] &&
-                 echo {} is a binary file ||
-                 (highlight -O ansi -l {} ||
-                  coderay {} ||
-                  rougify {} ||
-                  cat {}) 2> /dev/null | head -500'
-}
+# [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 
-function frmrf()
-{
-    rm -rf $(find $argv | fzf)
-}
+# function fzfv()
+# {
+#     fzf --preview '[[ $(file --mime {}) =~ binary ]] &&
+#         echo {} is a binary file ||
+#         (highlight -O ansi -l {} ||
+#         coderay {} ||
+#         rougify {} ||
+#         cat {}) 2> /dev/null | head -500'
+# }
+
+
+# function frmrf()
+# {
+#     rm -rf $(find $argv | fzf)
+# }
 
 # function fcd()
 # {
@@ -49,11 +59,11 @@ function frmrf()
 # }
 
 
-function fcd()
-{
-  local dir
-  dir=$(find ${1:-.} -path '*/\.*' -prune \
-                  -o -type d -print 2> /dev/null | fzf +m) &&
-  cd "$dir"
-}
+# function fcd()
+# {
+#     local dir
+#     dir=$(find ${1:-.} -path '*/\.*' -prune \
+#                     -o -type d -print 2> /dev/null | fzf +m) &&
+#     cd "$dir"
+# }
 # ==============================================================================
